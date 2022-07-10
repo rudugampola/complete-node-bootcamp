437728fc14c037b731e1c325959b5454c0e75fa0
rudugampola <ravindu365@gmail.com>
2022-07-10 15:13:46 -0700

    Updated

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -1,7 +1,24 @@
 /* eslint-disable prettier/prettier */
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+); // Replace the password in the connection string
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then((con) => {
+    console.log(con.connections);
+    console.log('DB Connection Successful');
+  });
+
 const app = require('./app');
 
 const port = process.env.PORT || 3000;
